5b10c8223ebe9486b2bc613c48a773ad3c474830
ahmedehab0 <ahmed.eh01@gmail.com>
2023-11-27 15:10:36 -0500

    reviews table

--- models/place.py
+++ models/place.py
@@ -4,21 +4,35 @@
 import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Integer, Column, String, ForeignKey, Float
-
+from sqlalchemy.orm import relationship
+from os import getenv
 
 class Place(BaseModel, Base):
     """place class"""
 
     __tablename__ = "places"
 
-    city_id = Column(String(60), ForeignKey('cities.id'), nullable = False)
-    user_id = Column(String(60), ForeignKey('users.id'), nullable = False)
-    name = Column(String(128), nullable = False)
-    description = Column(String(1024), nullable = True)
-    number_rooms = Column(Integer, nullable = False, default=0)
-    number_bathrooms = Column(Integer, nullable = False, default=0)
-    max_guest = Column(Integer, nullable = False, default=0)
-    price_by_night = Column(Integer, nullable = False, default=0)
-    latitude = Column(Float, nullable = True)
-    longitude = Column(Float, nullable = True)
     
+    if getenv('HBNB_TYPE_STORAGE') == "db":
+        city_id = Column(String(60), ForeignKey('cities.id'), nullable = False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable = False)
+        name = Column(String(128), nullable = False)
+        description = Column(String(1024), nullable = True)
+        number_rooms = Column(Integer, nullable = False, default=0)
+        number_bathrooms = Column(Integer, nullable = False, default=0)
+        max_guest = Column(Integer, nullable = False, default=0)
+        price_by_night = Column(Integer, nullable = False, default=0)
+        latitude = Column(Float, nullable = True)
+        longitude = Column(Float, nullable = True)
+        reviews = relationship("Review", backref="place")
+    else:
+        @property
+        def reviews(self):
+            reviews_list = []
+            all_reviews = models.storage.all(Review)
+
+            for review in all_reviews.values():
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+
+            return reviews_list
--- models/review.py
+++ models/review.py
@@ -2,12 +2,15 @@
 
 
 import models
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Integer, Column, String, ForeignKey
 
 
-class Review(BaseModel):
+class Review(BaseModel, Base):
     """review class"""
 
-    place_id = ""
-    user_id = ""
-    text = ""
+    __tablename__ = "reviews"
+
+    text = Column(String(1024), nullable = False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable = False)
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)

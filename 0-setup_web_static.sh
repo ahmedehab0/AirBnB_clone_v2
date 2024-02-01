d2d2ebdc1e349d978745fe776f7913a8ee7ae4d7
ahmedehab0 <ahmed.eh01@gmail.com>
2024-02-01 17:23:25 -0500

    setup web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+#bash script that sets up my web servers for the deployment
+
+apt-get update
+apt-get -y install nginx
+
+mkdir -p /data/web_static/shared/
+mkdir -p /data/web_static/releases/test/
+echo "<html>
+  <head>
+  </head>
+  <body>
+     Holeberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
+
+if [ -L "/data/web_static/releases/test" ];
+then
+	rm /data/web_static/releases/test
+fi
+
+ln -s /data/web_static/releases/test /data/web_static/current
+chown -R ubuntu:ubuntu /data/
+sed -i '51 i \\n\tlocation /hbnb_static {\n\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+
+service nginx restart
+
+

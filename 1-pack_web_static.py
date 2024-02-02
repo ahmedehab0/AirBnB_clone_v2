91354c3d142feec6c2c7a22228b0a43322b9c243
ahmedehab0 <ahmed.eh01@gmail.com>
2024-02-01 17:56:37 -0800

    fixed

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -14,11 +14,10 @@ def do_pack():
     date = datetime.now().strftime("%Y%m%d%H%M%S")
 
     local("mkdir -p versions")
-    filename = "web_static_{}.tgz".format(date)
-    result = local("tar -cvzf versions/{} web_static".format(filename))
-    exit_status = result.return_code 
-    if exit_status == 0:
-        return "versions/{}".format(filename)
+    filename = "versions/web_static_{}.tgz".format(date)
+    result = local("tar -cvzf {} web_static".format(filename))
+    if result.succeeded:
+        return filename
     else:
         return None
 

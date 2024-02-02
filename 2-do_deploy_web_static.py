7222172c3efbddef95a4155705d1f14c7c061f0b
ahmedehab0 <ahmed.eh01@gmail.com>
2024-02-02 16:07:54 -0500

    setup web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,23 +12,24 @@ env.user = 'ubuntu'
 env.use_ssh_config = True
 
 def do_deploy(archive_path):
-    """
-    deploy archive to wev servers
-    """
-
-    if exists(archive_path):
-        filename = archive_path.split('/')[1]
-        dest = '/data/web_static/releases/' + filename[:-4]
-        link = '/data/web_static/current/'
-        try:
-            put(archive_path, '/tmp/')
-            run('mkdir -p {}'.format(dest))
-            run('tar -xzvf {} -C {}'.format(archive_path, dest))
-            run('rm -f /tmp/{}'.format(filename))
-            run('rm -rf {}'.format(link))
-            run('ln -s {} {}'.format(dest, link))
-            return True
-        except:
-            return False
-    else:
+        '''
+    Deploy archive to web server
+    '''
+    if not os.path.exists(archive_path):
+        return False
+    file_name = archive_path.split('/')[1]
+    file_path = '/data/web_static/releases/'
+    releases_path = file_path + file_name[:-4]
+    try:
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}'.format(releases_path))
+        run('tar -xzf /tmp/{} -C {}'.format(file_name, releases_path))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {}/web_static/* {}/'.format(releases_path, releases_path))
+        run('rm -rf {}/web_static'.format(releases_path))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {} /data/web_static/current'.format(releases_path))
+        print('New version deployed!')
+        return True
+    except:
         return False

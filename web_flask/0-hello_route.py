8c53edfc9dd7ed06c3b2aa54287ca4871f1c4c87
ahmedehab0 <ahmed.eh01@gmail.com>
2024-02-17 14:32:15 -0500

    web flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,9 +1,17 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes = False)
 def HelloBnB():
+    """returns hello hbnb"""
+
     return "Hello HBNB!"
 
 if __name__ == '__main__':
--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,13 +1,23 @@
+#!/usr/bin/python3
+"""
+starts a Flask web application
+"""
+
+
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def HelloHBNB():
+    """returns hello hbnb"""
+
     return "Hello HBNB!"
 
 @app.route("/hbnb", strict_slashes=False)
 def HBNB():
+    """returns hbnb"""
+
     return "HBNB"
 
 

8690c410d9a741149666ee3f33aa1717ce215071
ahmedehab0 <ahmed.eh01@gmail.com>
2024-02-18 20:31:09 -0500

    web flask

--- web_flask/100-hbnb.py
+++ web_flask/100-hbnb.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template
 from models import storage
 from models.state import State
 from models.amenity import Amenity
-
+from models.place import Place
 app = Flask(__name__)
 
 @app.teardown_appcontext
@@ -18,7 +18,9 @@ def teardown(exception):
 def hbnb_filters():
     states= sorted(list(storage.all(State).values()), key = lambda x:x.name)
     amenities = sorted(list(storage.all(Amenity).values()), key = lambda x:x.name)
-    return render_template('100-hbnb.html', states=states, amenities=amenities)
+    places = sorted(list(storage.all(Place).values()), key = lambda x:x.name)
+
+    return render_template('100-hbnb.html', states=states, amenities=amenities, places=places)
 
 
 if __name__ == "__main__":
